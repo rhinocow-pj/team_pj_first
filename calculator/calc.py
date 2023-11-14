c6134ead9a6267931477075760001ab41074c70c
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-14 23:38:07 +0900

    Update calc.py

--- calculator/calc.py
+++ calculator/calc.py
@@ -6,11 +6,9 @@ def plus(a, b):
     result = a + b
     return result
 
-
 def minus(a, b):
     result = a - b
     return result
-
 
 def multi(a, b):
     result = a * b
@@ -24,22 +22,3 @@ def InPut():
             break
         infix.append(cur)
     return infix
-def Operands(infix):
-    numbers = list('0123456789.')
-    recognized = []
-
-    i = 0
-    while i < len(infix):
-        j = 1
-        if infix[i] in numbers:
-            while i + j < len(infix):
-                if infix[i + j] in numbers:
-                    j += 1
-                else:
-                    break
-            recognized.append(''.join(infix[i:i + j]))
-            i += j
-        else:
-            recognized.append(infix[i])
-            i += 1
-    return recognized
