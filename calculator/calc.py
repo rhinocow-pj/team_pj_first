024a71ce1d27fa0dfeec15ea172542640aa2331f
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-14 23:03:42 +0900

    Update calc.py

--- calculator/calc.py
+++ calculator/calc.py
@@ -3,26 +3,27 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 priority = {'(': 1, ')': 1, '+': 2, '-': 2, '*': 3, '/': 3}
 def plus(a, b):
-    a = int(a)
-    b = int(b)
     result = a + b
     return result
 
 
 def minus(a, b):
-    a = int(a)
-    b = int(b)
     result = a - b
     return result
 
 
 def multi(a, b):
-    a = int(a)
-    b = int(b)
     result = a * b
     return result
 
-
+def InPut():
+    infix = []
+    while(1):
+        cur = input()
+        if(cur == '='):
+            break
+        infix.append(cur)
+    return infix
 def Operands(infix):
     numbers = list('0123456789.')
     recognized = []
