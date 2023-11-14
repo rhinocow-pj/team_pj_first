283ea5aa56e6f48226a580897f86f61fbc3913eb
StackOverFly <81612749+rlarudans134@users.noreply.github.com>
2023-11-14 17:24:17 +0900

    Update calc.py
    try, except 삭제 -> main에서 판별

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,53 +1,41 @@
-import sys, os
+import os
+import sys
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-priority = {'(': 1, ')': 1, '+': 2, '-': 2, '*': 3, '/': 3}
 
-def plus(a,b):
-  try:
-    a = int(a)
-    b = int(b)
+def plus(a, b):
     result = a + b
     return result
-  except:
-    print(f"더하기 에러 발생")
-    return None
 
-def minus(a,b):
-  try:
-    a = int(a)
-    b = int(b)
+
+def minus(a, b):
     result = a - b
     return result
-  except:
-    print(f"빼기 에러 발생")
-    return None
 
-def multi(a,b):
-  try:
-    a = int(a)
-    b = int(b)
+
+def multi(a, b):
     result = a * b
     return result
-  except:
-    print(f"곱하기 에러 발생")
-    return None
-    
+
+
 def Operands(infix):
-  numbers = list('0123456789.')
-  recognized = []
-  i = 0
-  while i < len(infix):
-    j = 1
-    if infix[i] in numbers:
-      while i + j < len(infix):
-        if infix[i + j] in numbers:
-          j += 1
+    numbers = list('0123456789.')
+    recognized = []
+
+    i = 0
+    while i < len(infix):
+        j = 1
+        if infix[i] in numbers:
+            while i + j < len(infix):
+                if infix[i + j] in numbers:
+                    j += 1
+                else:
+                    break
+            recognized.append(''.join(infix[i:i + j]))
+            i += j
         else:
-          break
-      recognized.append(''.join(infix[i:i + j]))
-      i += j
-    else:
-      recognized.append(infix[i])
-      i += 1
-  return recognized
+            recognized.append(infix[i])
+            i += 1
+    return recognized
+
