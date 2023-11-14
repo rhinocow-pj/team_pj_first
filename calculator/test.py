a3294d30a2e10cf8defe83c05a3f1a34c96b42be
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-14 23:04:42 +0900

    Update test.py

--- calculator/test.py
+++ calculator/test.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from stack import stack
-from calc import plus, minus, multi, Operands
+from calc import plus, minus, multi, Operands, InPut
 from intopost import InToPost
 from postfix_calc import Postfix_calc
 from errorfinder import error_finder
@@ -26,11 +26,12 @@ from errorfinder import error_finder
 # print(Postfix_calc(postfix))
 
 while True:
- test_data = list(input("스택을 이용해 연산 숫서 결정하기위한 함수 테스팅 입력 예) (2+3)*4:" ))
- infix = Operands(test_data)
- postfix = InToPost(infix)
- print(postfix)
- print(Postfix_calc(postfix))
- if error_finder(test_data) != None:
-    print("수식에 오류가 있습니다! 수식을 다시 확인해주세요!")
-    continue
+     print("입력받기")
+     test_data = Operands(InPut())
+     infix = Operands(test_data)
+     postfix = InToPost(infix)
+     print(postfix)
+     print(Postfix_calc(postfix))
+     if error_finder(test_data) != None:
+        print("수식에 오류가 있습니다! 수식을 다시 확인해주세요!")
+        continue
