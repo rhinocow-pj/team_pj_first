bf5769529b19227bc927106a2bfff64084aea87b
hodu26 <69331491+hodu26@users.noreply.github.com>
2023-11-15 23:38:33 +0900

    Update easter_egg.py
    Update easter_egg to str

--- easter_egg/easter_egg.py
+++ easter_egg/easter_egg.py
@@ -4,18 +4,18 @@ import random
 # 삐삐호출
 def beep(result):
     messages = {
-        12486: "012486 : Forever love you!",
-        4486: "4486 : I love you even if I die!",
-        401: "401 : Love is forever!",
-        504: "504 : Only I love you!",
-        1052: "1052 : LOVE!",
-        1365244: "1365244 : I love you 24 hours a day, 365 days a year!",
-        17317071: "17317071 : I love you!",
-        1004: "1004 : You are angel!",
-        8282: "8282 : Hurry up!",
+        "12486" : "012486 : Forever love you!",
+        "4486" : "4486 : I love you even if I die!",
+        "401" : "401 : Love is forever!",
+        "504" : "504 : Only I love you!",
+        "1052" : "1052 : LOVE!",
+        "1365244" : "1365244 : I love you 24 hours a day, 365 days a year!",
+        "17317071" : "17317071 : I love you!",
+        "1004" : "1004 : You are angel!",
+        "8282" : "8282 : Hurry up!",
     }
 
-    result = int(result)
+    result = str(result)
 
     if result in messages:
         print(messages[result])
@@ -28,16 +28,16 @@ def beep(result):
 def birth(result):
     # 추가 예정
     list = {
-        202018388: "NOH",
-        202012237: "IM",
-        202118020: "KIM",
-        202012259: "HONG",
-        202018416: "HEO",
-        202012249: "JO",
-        202012259: "Park"
+        "202018388" : "NOH",
+        "202012237" : "IM",
+        "202118020" : "KIM",
+        "202012259" : "HONG",
+        "202018416" : "HEO",
+        "202012249" : "JO",
+        "201010613" : "Park"
     }
 
-    result = int(result)
+
 
     if result in list:
         print(list[result])
@@ -48,9 +48,10 @@ def birth(result):
 
 # 잭팟
 def jackpot(result):
-    result = int(result)
-    if result in [111, 222, 333, 444, 555, 666, 777, 888, 999]:
-        print(str(result) + " Jackpot!")
+    result = str(result)
+
+    if result in ["111", "222", "333", "444", "555", "666", "777", "888", "999"]:
+        print(result + " Jackpot!")
         return True
     else:
         return False
