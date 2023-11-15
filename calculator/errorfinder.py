d5a5fbea6bcd351bc8e83eaab88298b8cfde2966
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-11-15 23:51:25 +0900

    Update errorfinder.py

--- calculator/errorfinder.py
+++ calculator/errorfinder.py
@@ -8,12 +8,12 @@ def error_finder(test_data):
 
     for i in range(len(test_data)):
         if test_data[i] not in input_data:
-            return "존재하지 않는 값입니다."
+            return "입력은 정수 또는 지정된 연산자(+,-,*)만 입력가능합니다."
         elif i == 0 and test_data[i] in operators:
             return "연산자가 처음으로 입력되었습니다."
         elif test_data[i] in operators:
             if i + 1 == len(test_data):
-                return "오버플로우"
+                return "입력을 제대로 해주세요."
             else:
                 if test_data[i + 1] in operators:
                     return "연산자 중복오류"
