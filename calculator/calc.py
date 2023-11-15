b44b4044eb049adde6461f37c3f3d26c24d3bf16
hodu26 <69331491+hodu26@users.noreply.github.com>
2023-11-15 23:21:46 +0900

    Update calc.py
    Update easteregg to print immediately 33 (InPut())

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
+from easter_egg import easter_egg
+
 priority = {'(': 1, ')': 1, '+': 2, '-': 2, '*': 3, '/': 3}
 def plus(a, b):
     result = a + b
@@ -21,9 +23,11 @@ def InPut():
     k = 0
     while(1):
         cur = input()
+        if cur[0] in integers and (easter_egg.beep(cur) or easter_egg.birth(cur) or easter_egg.jackpot(cur)):
+            return "easter_egg"
         if k != 0 and infix[k-1] in integers and cur[0] in integers:
-            return []
-        if(cur == '='):
+            return "error"
+        if cur == '=':
             break
         for i in cur:
             infix.append(i)
