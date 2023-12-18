17d21d068cf148c14a0eecf7d4daa72d5a8da024
highcastle01 <76648806+highcastle01@users.noreply.github.com>
2023-12-18 22:37:34 +0900

    Update unit_testing.py

--- unit_testing.py
+++ unit_testing.py
@@ -1,37 +1,37 @@
-from calculator import calc
+from calculator import calc, factorial
 from stack import stack
 import unittest
 
 def error_finder(test_data):
-    input_data = list('0123456789()+-*')
+    input_data = list('0123456789()+-*!')
     integers = list('0123456789')
     operators = list('+-*')
     s = stack.Stack()
 
     for i in range(len(test_data)):
         if test_data[i] not in input_data:
-            return "입력은 정수 또는 지정된 연산자(+,-,*)만 입력가능합니다."
+            return "[SYSTEM]입력은 정수 또는 지정된 연산자(+,-,*)만 입력가능합니다."
         elif i == 0 and test_data[i] in operators:
-            return "연산자가 처음으로 입력되었습니다."
+            return "[SYSTEM]연산자가 처음으로 입력되었습니다."
         elif test_data[i] in operators:
             if i + 1 == len(test_data):
-                return "입력을 제대로 해주세요."
+                return "[SYSTEM]입력을 제대로 해주세요."
             else:
                 if test_data[i + 1] in operators:
-                    return "연산자 중복오류"
+                    return "[SYSTEM]연산자 중복오류"
         elif test_data[i] == '(':
             if test_data[i - 1] in integers and i != 0:
-                return "괄호처리 오류"
+                return "[SYSTEM]괄호처리 오류"
             else:
                 if s.is_empty():
                     s.push('(')
                 else:
-                    return "괄호처리 오류"
+                    return "[SYSTEM]괄호처리 오류"
         elif test_data[i] == ')':
             if s.is_empty():
-                return "괄호처리 오류"
+                return "[SYSTEM]괄호처리 오류"
             elif test_data[i + 1] in integers and i != len(test_data) - 1:
-                return "괄호처리 오류"
+                return "[SYSTEM]괄호처리 오류"
             else:
                 s.push(')')
 
@@ -52,7 +52,7 @@ def student_id(result):
     }
 
     if result in list:
-        #유닛테스팅을 위한 코드 수정. 기존 : print(list[result])
+        #유닛테스팅을 위한 코드 수정. 기존 : print("[EVENT]"+list[result])
         return True
     else:
         return False
@@ -62,6 +62,11 @@ def Postfix_calc(postfix):
     for cur in postfix:
         if cur in calc.priority:
             operands1 = int(st.pop())
+
+            if cur == '!':
+                st.push(factorial.factorial(operands1))
+                continue
+
             operands2 = int(st.pop())
             if cur == '+':
                 st.push(calc.plus(operands2, operands1))
@@ -76,13 +81,13 @@ def Postfix_calc(postfix):
 class TestCapErrorFinder(unittest.TestCase):
     def test_error_finder_1(self):
         input = "5/9+4"
-        self.assertEqual("입력은 정수 또는 지정된 연산자(+,-,*)만 입력가능합니다.", error_finder(input))
+        self.assertEqual("[SYSTEM]입력은 정수 또는 지정된 연산자(+,-,*)만 입력가능합니다.", error_finder(input))
     def test_error_finder_2(self):
         input = "+9*11"
-        self.assertEqual("연산자가 처음으로 입력되었습니다.", error_finder(input))
+        self.assertEqual("[SYSTEM]연산자가 처음으로 입력되었습니다.", error_finder(input))
     def test_error_finder_3(self):
         input = "9*+11"
-        self.assertEqual("연산자 중복오류", error_finder(input))
+        self.assertEqual("[SYSTEM]연산자 중복오류", error_finder(input))
 
 class TestCapStudentnumber(unittest.TestCase):
     def test_student_number_1(self):
@@ -105,6 +110,10 @@ class TestCapPostToFix(unittest.TestCase):
         input = "853*-9+"
         result = Postfix_calc(input)
         self.assertEqual(result, 2)
+    def test_error_finder_fact(self):
+        input = "4!"
+        result = Postfix_calc(input)
+        self.assertEqual(result, 24)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
